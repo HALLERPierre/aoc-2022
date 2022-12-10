6afa246e39ee8e3092829f19f007505f992109e1
Pierre Haller <pierre.haller@datalegaldrive.com>
2022-12-10 14:41:07 +0100

    day 7

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,3 +1,22 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *			RAGEQUIT ON RUST, DONE IN TS
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
 const INPUT: &str = include_str!("./input");
 
 use lazy_static::lazy_static;
@@ -26,22 +45,33 @@ impl Directory {
 
 pub fn puzzle1() {
     let root_directory = init_root_directory();
-    compute_size(Rc::clone(&root_directory));
-    {
-        println!("{}", root_directory.borrow().size.unwrap());
-    }
+
+    let clone = Rc::clone(&root_directory);
+    compute_size(clone);
+    println!("FAILED");
+}
+
+pub fn puzzle2() {
+    println!("FAILED");
 }
 
 fn compute_size(directory: Rc<RefCell<Directory>>) {
-    for sub_dir in directory.borrow().children.iter() {
+    let clone = Rc::clone(&directory);
+
+    for sub_dir in clone.borrow().children.iter() {
         compute_size(Rc::clone(sub_dir));
     }
-    directory.borrow_mut().size = directory
-        .borrow()
-        .children
+
+    let children = Rc::clone(&Rc::new(clone.borrow().children.to_vec()));
+    let files = Rc::clone(&Rc::new(clone.borrow().files.to_vec()));
+
+    let sub_dirs_size: i32 = children
         .iter()
-        .map(|dir| dir.borrow().size)
+        .map(|dir| dir.borrow().size.unwrap_or(0))
         .sum();
+    let files_size: i32 = files.iter().sum();
+
+    clone.borrow_mut().size = Some(sub_dirs_size + files_size);
 }
 
 fn init_root_directory() -> Rc<RefCell<Directory>> {
@@ -49,46 +79,43 @@ fn init_root_directory() -> Rc<RefCell<Directory>> {
 
     let root = Rc::new(RefCell::new(Directory::new()));
     let mut current = Rc::clone(&root);
-    let mut indent = 0;
-    for (index, line) in lines.enumerate() {
+    for line in lines {
         {
             println!("{}", line);
         }
-        if index == 0 {
+        if line == "$ cd /" || line.starts_with("dir") || line == "$ ls" {
             continue;
         }
-        let current_indent = line.find("-").expect("should have '-'");
 
-        // return to parent
-        if current_indent < indent {
-            let current_clone = Rc::clone(&current);
-            current = Rc::clone(
-                current_clone
-                    .borrow()
-                    .parent
-                    .as_ref()
-                    .expect("should have parent"),
-            );
-        }
-
-        // is child directory
-        if line.contains("dir") {
-            let child = Rc::new(RefCell::new(Directory::new()));
-            current.borrow_mut().children.push(Rc::clone(&child));
-            {
-                let mut mut_child = child.borrow_mut();
-                mut_child.parent = Some(Rc::clone(&current));
+        if line.starts_with("$ cd") {
+            // return to parent
+            if line == "$ cd .." {
+                let current_clone = Rc::clone(&current);
+                current = Rc::clone(
+                    current_clone
+                        .borrow()
+                        .parent
+                        .as_ref()
+                        .expect("should have parent"),
+                );
             }
-            current = child;
+            // Going into child dir
+            else {
+                let child = Rc::new(RefCell::new(Directory::new()));
+                current.borrow_mut().children.push(Rc::clone(&child));
+                {
+                    let mut mut_child = child.borrow_mut();
+                    mut_child.parent = Some(Rc::clone(&current));
+                }
+                current = child;
+            }
         }
 
         // it's a file
-        if line.contains("file") {
+        if !line.starts_with("$") {
             let file_size = find_file_size(line);
             current.borrow_mut().files.push(file_size);
         }
-
-        indent = current_indent;
     }
 
     return root;
@@ -96,14 +123,10 @@ fn init_root_directory() -> Rc<RefCell<Directory>> {
 
 fn find_file_size(line: &str) -> i32 {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"size=(\d+)").unwrap();
+        static ref RE: Regex = Regex::new(r"(\d+)").unwrap();
     }
 
     let captures = RE.captures(line).expect("should have number");
 
     return captures[1].parse::<i32>().expect("should be int");
-}
-
-pub fn puzzle2() {
-    println!("WIP");
 }
